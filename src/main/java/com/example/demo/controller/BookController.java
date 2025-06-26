0422d5d76390975d7a0646abb92eb4ba828e3df0
woojinkong <dodejqn6@gmail.com>
2025-06-26 15:28:36 +0900

    <feat> insert add

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,12 @@ public class BookController {
 	}
 	
 	
+	 // 📌 도서 등록 API
+    @PostMapping
+    public Book insert(@RequestBody Book book) {
+        return dao.save(book); // 저장 후 저장된 book 객체 반환 (id 포함)
+    }
+   
 
 	
 	
